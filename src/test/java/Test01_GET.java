e9bc63f3d05aebac498d3e66f08708cf08c724e3
parveenkhan1 <parveensultanauk@gmail.com>
2023-06-11 23:07:20 +0100

    Added a test to verify the list of places

--- src/test/java/Test01_GET.java
+++ src/test/java/Test01_GET.java
@@ -5,6 +5,8 @@ import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -64,6 +66,44 @@ public class Test01_GET {
         //Print the response from second API call
         System.out.println("Second API Response");
         System.out.println(response2.asString());
+
+    }
+
+    @Test
+    void requestToVerifyTheList(){
+        //Make the api request
+        //RestAssured.baseURI = "http://api.zippopotam.us/us/12345";
+        Response response = RestAssured.get("http://api.zippopotam.us/us/12345");
+
+        //Extract the state abbreviation and place name from the response
+
+        String stateAbbreviation = response.path("places[0].'state abbreviation'");
+        String placeName = response.path("places[0].'place name'");
+
+        //Print the extracted values
+        System.out.println("State Abbreviation:" + stateAbbreviation);
+        System.out.println("Place Name :" + placeName);
+
+        //Store the extracted values in variables
+        String stateAbbreviationVariable = stateAbbreviation;
+        String  placeNameVariable = placeName;
+
+        //Use the stored values as path parameters in the second API Call
+        Response response2 = RestAssured.get("http://api.zippopotam.us/us/" + stateAbbreviationVariable + "/" + placeNameVariable);
+
+        //Get the list of places from the second api response
+        List<Object> places = response2.path("places");
+
+        //Verify the number of entries in the places list
+        int numberOfPlaces = places.size();
+        System.out.println("Number of places : " + numberOfPlaces);
+
+        //Check if the number of places is 11
+        if(numberOfPlaces == 11){
+            System.out.println("The list of places has 11 entries");
+        } else {
+            System.out.println("The list of places does not have " + numberOfPlaces + " entries");
+        }
 
     }
 
